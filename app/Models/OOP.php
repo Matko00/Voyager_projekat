64f7c1537c2e27cee62ed5538252b095e95c50c7
Matko00 <89593439+Matko00@users.noreply.github.com>
2023-03-15 14:17:14 +0100

    Add files via upload

---
+++ app/Models/OOP.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OOP extends Model
+{
+    use HasFactory;
+    //adfsfasfasf
+    
+}
