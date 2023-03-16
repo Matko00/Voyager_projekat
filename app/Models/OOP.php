46d2c2dcb0531a1ee83e508ea467c486e7526107
Your Name01~ <you@example.com>
2023-03-16 12:15:44 +0100

    proba

--- app/Models/OOP.php
+++ app/Models/OOP.php
@@ -3,3 +3,11 @@
 //ovo je prva promena
 //ovo cu kasnije da merge sa main
 //Dodaj nesto
+<<<<<<< HEAD
+=======
+//Izmena
+//Jos teksta
+//hello, hello
+//cao cao cao
+//ovo je proba
+>>>>>>> 69454d2 (proba)
